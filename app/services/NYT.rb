e3819c5c778b73f194c32de5492ed18bfa998fed
Sam Landfried <samlandfried@gmail.com>
2020-02-22 17:54:44 -0700

    Call NYT API
    Request failing because API key isn't being loaded correctly from credentials

--- app/services/NYT.rb
+++ app/services/NYT.rb
@@ -2,9 +2,25 @@
 
 # Fetches articles from the NYT API and extracts sentences out of them
 class NYT
-  @url = 'https://api.nytimes.com/'
+  attr_accessor :articles
+
+  def initialize
+    @url = 'https://api.nytimes.com/'
+  end
 
   def make_sentence
-    'A sentence from an NYT article'
+    print fetch_popular_articles
+  end
+
+  private
+
+  def popular_articles_endpoint(days = 7)
+    "#{@url}svc/mostpopular/v2/emailed/#{days}.json"
+  end
+
+  def fetch_popular_articles
+    print ENV['NYT_API_KEY']
+    articles = Faraday.get(popular_articles_endpoint, { 'api-key': ENV['NYT_API_KEY'] })
+    articles.body
   end
 end
