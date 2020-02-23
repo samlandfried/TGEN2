b5d6a4d13a4b97ec14d1ade7e79c9b6825d42203
Sam Landfried <samlandfried@gmail.com>
2020-02-23 15:22:55 -0700

    Find testable words in a sentence

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,8 +1,37 @@
 # frozen_string_literal: true
 
+FREQUENCY_THRESHOLD = 4.0
+LENGTH_THRESHOLD = 3
+
 class Sentence < ApplicationRecord
   validates :original, presence: true
   validates_uniqueness_of :original
 
   has_many :questions, dependent: :destroy
+
+  def testable_words
+    word_details.each_with_index.each_with_object([]) do |(word, i), memo|
+      memo.push(i) if low_frequency?(word) && long_enough?(word)
+      memo
+    end
+  end
+
+  private
+
+  def just_words
+    original.gsub(/[^a-z ]/i, '').split(' ')
+  end
+
+  def word_details
+    @words ||= Words.new
+    just_words.map { |word| @words.word(word) }
+  end
+
+  def low_frequency?(word)
+    word[:frequency] < FREQUENCY_THRESHOLD if word[:frequency]
+  end
+
+  def long_enough?(word)
+    word[:word].length > LENGTH_THRESHOLD
+  end
 end
--- spec/models/sentence_spec.rb
+++ spec/models/sentence_spec.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'vcr_helper'
 
 RSpec.describe Sentence, type: :model do
   context 'valid Sentences' do
     it 'requires an "original" field' do
-      expect(Sentence.create).to be_invalid
-      expect(Sentence.create({ original: 'Original sentence' })).to be_valid
+      expect(Sentence.new).to be_invalid
+      expect(Sentence.new({ original: 'Original sentence' })).to be_valid
     end
 
     it 'requires a unique "original" field' do
       expect(Sentence.create({ original: 'Original sentence' })).to be_valid
-      expect(Sentence.create({ original: 'Original sentence' })).to_not be_valid
+      expect(Sentence.new({ original: 'Original sentence' })).to_not be_valid
+    end
+  end
+
+  context 'testability', :vcr do
+    it 'marks words with low frequency as testable' do
+      sentence = Sentence.create(original: 'skeptical happy eccentric common')
+      expect(sentence.testable_words).to eq([0, 2])
+    end
+
+    it 'marks words longer than 3 characters as testable' do
+      sentence = Sentence.create(original: 'ire scrutiny')
+      expect(sentence.testable_words).to eq([1])
     end
   end
 end
