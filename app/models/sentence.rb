465cdc01b65df004b88e973f0a4e467bc432e39b
Sam Landfried <samlandfried@gmail.com>
2020-02-23 11:07:06 -0700

    Require 'original' on sentences

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Sentence < ApplicationRecord
+  validates :original, presence: true
 end
--- db/migrate/20200223175427_create_sentences.rb
+++ db/migrate/20200223175427_create_sentences.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreateSentences < ActiveRecord::Migration[6.0]
   def change
     create_table :sentences do |t|
-      t.string :original
+      t.string :original, :unique
 
       t.timestamps
     end
