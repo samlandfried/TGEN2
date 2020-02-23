84aced52ac095e7e44256f72e6dedf5a41d3d560
Sam Landfried <samlandfried@gmail.com>
2020-02-23 14:00:45 -0700

    Validate questions have sentences

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'requires a sentence' do
+    expect(Question.new).to_not be_valid
+    sentence = Sentence.create(original: 'New sentence')
+    expect(Question.new(sentence: sentence)).to be_valid
+  end
 end
