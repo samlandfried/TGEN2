0be6bc521bd53635bf1a9868cec2bf55eb29a385
Sam Landfried <samlandfried@gmail.com>
2020-02-23 12:54:42 -0700

    Save sentences

--- spec/requests/sentences_request_spec.rb
+++ spec/requests/sentences_request_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe 'Sentences', :vcr, type: :request do
     expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(JSON.parse(response.body).length).to eq(20)
   end
+
+  it 'saves sentence' do
+    sentences = NYT.new.sentences
+    post '/sentences', params: { sentence: { original: sentences.first } }
+
+    expect(response).to have_http_status(:created)
+    expect(Sentence.all.length).to eq(1)
+    expect(Sentence.first.original).to eq(sentences.first)
+  end
 end
