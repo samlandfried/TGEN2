bbf0c1cff0203c3885cc62bcb4d794188428397a
Sam Landfried <samlandfried@gmail.com>
2020-02-23 18:12:28 -0700

    Randomly choose testable word for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,12 @@
 class Question < ApplicationRecord
   belongs_to :sentence
   has_many :options
+
+  before_create :assign_word_under_test
+
+  private
+
+  def assign_word_under_test
+    self.word_under_test = sentence.testable_words.sample
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,10 +3,18 @@
 require 'rails_helper'
 require 'vcr_helper'
 
+NEW_SENTENCE = 'New phenomenal sentence'
+
 RSpec.describe Question, :vcr, type: :model do
   it 'requires a sentence' do
     expect(Question.new).to_not be_valid
-    sentence = Sentence.create(original: 'New phenomenal sentence')
+    sentence = Sentence.create(original: NEW_SENTENCE)
     expect(Question.new(sentence: sentence)).to be_valid
+  end
+
+  it 'chooses a random testable word if none is specified' do
+    sentence = Sentence.create(original: NEW_SENTENCE)
+    question = Question.create(sentence: sentence)
+    expect(question.word_under_test).to eq(1)
   end
 end
