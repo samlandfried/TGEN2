07bedc11438fe151518c70cc3ada7c0a32a28a46
Sam Landfried <samlandfried@gmail.com>
2020-03-19 09:03:09 -0600

    Generate words service in before block

--- spec/services/words_spec.rb
+++ spec/services/words_spec.rb
@@ -4,9 +4,12 @@ require 'rails_helper'
 require 'vcr_helper'
 
 RSpec.describe Words, :vcr do
+  before_each do
+    @words = Words.new
+  end
+
   it 'can get info on a word' do
-    words = Words.new
-    hatchback_details = words.word('hatchback')
+    hatchback_details = @words.word('hatchback')
     expect(hatchback_details[:word]).to eq('hatchback')
   end
 end
