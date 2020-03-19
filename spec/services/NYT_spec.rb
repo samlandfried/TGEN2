2d3fd32f4c12cb930d298a978091904e11c50acf
Sam Landfried <samlandfried@gmail.com>
2020-03-19 17:50:03 -0600

    Update test

--- spec/services/NYT_spec.rb
+++ spec/services/NYT_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe NYT, :vcr do
   it 'can produce sentences' do
     nyt = NYT.new
     expect(nyt.sentences.length).to eq(20)
-    expect(nyt.sentences.first).to eq('I was skeptical, but it turns out there is something to be said for a daily fast, preferably one lasting at least 16 hours.')
-    expect(nyt.sentences.last).to eq('The $13 billion deal will give a powerful Wall Street firm control of a major presence in the world of online brokerage firms.')
+    expect(nyt.sentences.first).to eq('At the molecular level, soap breaks things apart. At the level of society, it helps hold everything together.')
+    expect(nyt.sentences.last).to eq('This once-in-a-century pandemic is hitting a world economy saddled with record levels of debt.')
   end
 end
