410e3fc511fac7aaa75ad02a1304345f7e50ebef
Sam Landfried <samlandfried@gmail.com>
2020-03-19 17:56:20 -0600

    Seed DB with questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sentences = NYT.new.sentences
+
+sentences.each do |sentence|
+  begin
+    sent = Sentence.create(original: sentence)
+    Question.create(sentence: sent)
+  rescue => exception
+    p 'Failed to create question for:'
+    p sent
+    p exception
+  end
+end
