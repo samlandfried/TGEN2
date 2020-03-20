e82acb8a0fd869d1502fc69f20a56883d9226fe2
Sam Landfried <samlandfried@gmail.com>
2020-03-19 18:21:44 -0600

    Create questions route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :questions, only: %i[create show]
+  resources :questions, only: %i[index create]
   resources :sentences, only: %i[index create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
