4d648784defca4460d4b27f5e1130b7727984148
Sam Landfried <samlandfried@gmail.com>
2020-03-21 12:55:35 -0600

    Rename root component to App

---
+++ app/javascript/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+class App extends React.Component {
+  render() {
+    return <React.Fragment>TGEN2</React.Fragment>;
+  }
+}
+
+App.propTypes = {
+  greeting: PropTypes.string
+};
+export default App;
