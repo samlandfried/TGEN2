0d0f113d5d05f967cc008a41b5343e7cc7de5381
Sam Landfried <samlandfried@gmail.com>
2020-03-21 16:55:51 -0600

    Style Question

--- app/javascript/components/Question.js
+++ app/javascript/components/Question.js
@@ -3,14 +3,16 @@ import styles from "./Question.module.scss";
 
 const Question = ({ question, options }) => (
   <div className={styles.container}>
-    <div className={styles.question}>{question}</div>
-    <ol className={styles.options}>
-      {options.map(option => (
-        <li key={option} className={styles.option}>
-          {option}
-        </li>
-      ))}
-    </ol>
+    <div className={styles.contents}>
+      <div className={styles.question}>{question}</div>
+      <ol className={styles.options}>
+        {options.map(option => (
+          <li key={option} className={styles.option}>
+            {option}
+          </li>
+        ))}
+      </ol>
+    </div>
   </div>
 );
 
