a0f987080b90743c6479efdcbaca894666037ec5
Sam Landfried <samlandfried@gmail.com>
2020-03-22 09:45:14 -0600

    Normalize blank size

--- app/javascript/components/Question.js
+++ app/javascript/components/Question.js
@@ -2,17 +2,39 @@ import React, { useState, useEffect } from "react";
 import { CSSTransitionGroup } from "react-transition-group";
 import styles from "./Question.module.scss";
 
+const _normalizeOptions = options => {
+  const lengthOfLongestOption = options.reduce(
+    (longest, current) => (current.length > longest ? current.length : longest),
+    0
+  );
+
+  return options.map(option => {
+    while (option.length < lengthOfLongestOption) {
+      option = `${option} `;
+    }
+
+    return option;
+  });
+};
+
 const Question = ({ question, options }) => {
   const questionWords = question.split(" ");
   const blankIndex = questionWords.indexOf("___");
-  const [formattedQuestion, setFormattedQuestion] = useState(question);
-  const [hoveredOption, setHoveredOption] = useState();
+  const normalizedOptions = _normalizeOptions(options);
+  const BLANK = Array(normalizedOptions[0].length)
+    .fill("_")
+    .join("");
+  const [formattedQuestion, setFormattedQuestion] = useState();
+  const [hoveredOption, setHoveredOption] = useState(BLANK);
 
   useEffect(() => {
-    const valueForBlank = hoveredOption || "_____";
-    questionWords.splice(blankIndex, 1, valueForBlank);
+    const firstWordGroup = questionWords.slice(0, blankIndex);
+    const valueForBlank = hoveredOption || BLANK;
+    const secondWordGroup = questionWords.slice(blankIndex + 1);
 
-    setFormattedQuestion(questionWords.join(" "));
+    setFormattedQuestion(
+      [firstWordGroup, valueForBlank, secondWordGroup].flat().join(" ")
+    );
   }, [hoveredOption]);
 
   return (
@@ -29,14 +51,17 @@ const Question = ({ question, options }) => {
           </p>
         </CSSTransitionGroup>
         <ol className={styles.options}>
-          {options.map(option => (
+          {options.map((option, optionI) => (
             <li
               key={option}
               className={styles.option}
-              onMouseOver={({ target }) => setHoveredOption(target.innerText)}
-              onMouseOut={() => setHoveredOption("_____")}
+              onMouseOver={({ target }) => {
+                const index = Number(target.dataset.id);
+                setHoveredOption(normalizedOptions[index]);
+              }}
+              onMouseOut={() => setHoveredOption(BLANK)}
             >
-              <button>{option}</button>
+              <button data-id={optionI}>{option}</button>
             </li>
           ))}
         </ol>
