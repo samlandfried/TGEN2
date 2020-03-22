7f90a5cc8cd4f67f14dd86a9264a9a0b7b9c1374
Sam Landfried <samlandfried@gmail.com>
2020-03-22 12:08:52 -0600

    Add check answer endpoint

---
+++ app/controllers/check_answer_controller.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class CheckAnswerController < ApplicationController
+  def create
+    question = Question.find(question_id)
+    check_answer_code = question.check_answer(answer_param)
+
+    case check_answer_code
+    when 'CORRECT'
+      render json: { code: check_answer_code, message: "Option '#{answer_param}' completes the sentence." }, status: :ok
+    when 'INCORRECT'
+      render json: { code: check_answer_code, message: "Option '#{answer_param}' does not complete the sentence."}, status: :ok
+    when 'NOT_FOUND'
+      render json: { code: check_answer_code, message: "Option '#{answer_param} is not associated with the question"}, status: :not_found
+    else
+      render nothing: true, status: :system_error
+    end
+  end
+
+  private
+
+  def check_answer_params
+    params.require(:answer)
+  end
+
+  def answer_param
+    check_answer_params.require(:word)
+  end
+
+  def question_id
+    params.require(:question_id)
+  end
+end
---
+++ spec/requests/check_answer_request_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'vcr_helper'
+
+RSpec.describe 'Check Answer', :vcr, type: :request do
+  before :each do
+    sentence = Sentence.create(original: 'A mesmerizing sentence.')
+    @question = Question.create(sentence: sentence)
+  end
+
+  it 'checks correct answers' do
+    answer = @question.correct.name
+
+    post "/questions/#{@question.id}/check_answer", { params: { answer: { word: answer } } }
+
+    expect(response).to have_http_status(:ok)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body[:code]).to eq 'CORRECT'
+    expect(body[:message]).to eq "Option '#{answer}' completes the sentence."
+  end
+
+  it 'checks incorrect answers' do
+    answer = @question.options.find_by(correct: false).name
+
+    post "/questions/#{@question.id}/check_answer", { params: { answer: { word: answer } } }
+
+    expect(response).to have_http_status(:ok)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body[:code]).to eq 'INCORRECT'
+    expect(body[:message]).to eq "Option '#{answer}' does not complete the sentence."
+  end
+
+  it '404s when an answer is not found on question options' do
+    answer = 'Not found'
+
+    post "/questions/#{@question.id}/check_answer", { params: { answer: { word: answer } } }
+
+    expect(response).to have_http_status(:not_found)
+  end
+end
