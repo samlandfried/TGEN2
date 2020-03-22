ca4bf890775640c96677d4967dbc9efb7b777d39
Sam Landfried <samlandfried@gmail.com>
2020-03-21 18:09:23 -0600

    Insert hovered word in sentence

--- app/javascript/components/Question.js
+++ app/javascript/components/Question.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Question.module.scss";
 
-const Question = ({ question, options }) => (
-  <div className={styles.container}>
-    <div className={styles.contents}>
-      <div className={styles.question}>{question}</div>
-      <ol className={styles.options}>
-        {options.map(option => (
-          <li key={option} className={styles.option}>
-            <button>{option}</button>
-          </li>
-        ))}
-      </ol>
+const Question = ({ question, options }) => {
+  const questionWords = question.split(" ");
+  const blankIndex = questionWords.indexOf("___");
+  const [formattedQuestion, setFormattedQuestion] = useState(question);
+  const [hoveredOption, setHoveredOption] = useState();
+
+  useEffect(() => {
+    const valueForBlank = hoveredOption || "_____";
+    questionWords.splice(blankIndex, 1, valueForBlank);
+
+    setFormattedQuestion(questionWords.join(" "));
+  }, [hoveredOption]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.contents}>
+        <div className={styles.question}>{formattedQuestion}</div>
+        <ol className={styles.options}>
+          {options.map(option => (
+            <li
+              key={option}
+              className={styles.option}
+              onMouseOver={({ target }) => setHoveredOption(target.innerText)}
+              onMouseOut={() => setHoveredOption("_____")}
+            >
+              <button>{option}</button>
+            </li>
+          ))}
+        </ol>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Question;
